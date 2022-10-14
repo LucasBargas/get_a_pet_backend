76e39684c9f5d340df71c39eab3c3bb4bd424779
LucasBargas <lucas.bargasoficial@gmail.com>
2022-10-14 19:28:10 -0300

    project corrected

--- helpers/check-token.js
+++ helpers/check-token.js
@@ -4,12 +4,17 @@ const getToken = require('./get-token');
 
 // middleware to validate token
 const checkToken = (req, res, next) => {
-  if (!req.headers.authorization)
-    return res.status(401).json({ message: 'Acesso negado!' });
+  if (!req.headers.authorization) {
+    res.status(401).json({ message: 'Acesso negado!' });
+    return;
+  }
 
   const token = getToken(req);
 
-  if (!token) return res.status(401).json({ message: 'Acesso negado!' });
+  if (!token) {
+    res.status(401).json({ message: 'Acesso negado!' });
+    return;
+  }
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
